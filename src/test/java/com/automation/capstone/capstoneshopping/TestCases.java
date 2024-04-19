9288efc12183bbed185fec36f53b912b1330d3d4
hpsridevi <sridevi.hp@brillio.com>
2024-04-19 17:29:26 +0530

    test

--- src/test/java/com/automation/capstone/capstoneshopping/TestCases.java
+++ src/test/java/com/automation/capstone/capstoneshopping/TestCases.java
@@ -9,7 +9,7 @@ public class TestCases extends BaseTest{
 	@Test(priority = 1)
     public void fieldErrors() {
 	  
-		AccountCreationError flderror   = new AccountCreationError(driver, log, test, report);
+		AccountCreationError flderror   = new AccountCreationError(driver, log, test, report, softassert);
 		flderror.mandatoryFldValidation();	 
 	  
   }
@@ -18,7 +18,7 @@ public class TestCases extends BaseTest{
   @Test(priority = 2)
   public void testCreateUserSuccess() {
 	  
-	  CreateNewUser newuser   = new CreateNewUser(driver, log, test, report);
+	  CreateNewUser newuser   = new CreateNewUser(driver, log, test, report, softassert);
 	  newuser.newUserCreation();	 
 	  
   }
@@ -26,14 +26,20 @@ public class TestCases extends BaseTest{
   @Test(priority = 3)
     public void loginCreatedUser() {
 	  
-	  Login login   = new Login(driver, prop, log, wait, test, report);
+	  Login login   = new Login(driver, prop, log, wait, test, report, softassert);
 	  login.loginToCapstone();	 
 	  
   }
   
   @Test(dependsOnMethods ="loginCreatedUser")
+  public void addToCart() {
+	  AddToCart addToCart   = new AddToCart(driver, act, js, wait, log, test, report, softassert);
+	  addToCart.addItems();
+}
+  
+  @Test(dependsOnMethods = {"loginCreatedUser","addToCart"})
   public void viewCart() {
-	  ViewCart viewCart   = new ViewCart(driver, act, js, wait, log, test, report);
+	  ViewCart viewCart   = new ViewCart(driver, act, js, wait, log, test, report, softassert);
 	  viewCart.cartItemsCheck();	 
 	  viewCart.qtyChange();
 	  viewCart.removeItem();
